0824bb4e4b868f66e95fc3d9f950893e683d5aa2
Pendolf1001 <kovalev8915512@gmail.com>
2024-02-24 15:50:55 +0300

    add some variebles lesson1:Task1

---
+++ task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console.WriteLine("World of tanks");
+
+
+
+int value=100;
+Console.WriteLine(value);
+
+
+double d=5.5;
+char c='!';
+bool b= true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
